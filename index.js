1f1d555e029b28d7e6ad2b76943ec39aeb259ddf
codywall <cwall@csumb.edu>
2020-06-19 11:53:52 -0700

    Get it working for a single page with text only

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
 'use strict';
-
 const fs = require('fs');
 
 let rawData = fs.readFileSync('pages.json');
-
-// fs.writeFileSync('file.json', pages, (err) => {
-//   console.log(err);
-// });
-
 let pages = JSON.parse(rawData).pages;
 let htmlContent = [];
+// for (let i = 0; i < pages.length; i++) {
 let content = JSON.parse(pages[0].node.pageContent);
-console.log(content);
 let values = Object.values(content.blocks);
-console.log(values);
-console.log(values[0].type);
-for (let i = 0; i < values.length; i++) {
-  if (values[i].type == 'heading') {
-    let header = values[i].data.url
-      ? `<h${values[i].data.level}><a href='${values[i].data.url}'>${values[i].data.text}</a></h${values[i].data.level}>`
-      : `<h${values[i].data.level}>${values[i].data.text}</h${values[i].data.level}>`;
+let title = pages[0].node.title;
+let fileName = title.replace(/\s+/g, '-').toLowerCase();
+
+for (let j = 0; j < values.length; j++) {
+  if (values[j].type == 'heading') {
+    let header = values[j].data.url
+      ? `<h${values[j].data.level}><a href='${values[j].data.url}'>${values[j].data.text}</a></h${values[i].data.level}>`
+      : `<h${values[j].data.level}>${values[j].data.text}</h${values[j].data.level}>`;
 
     htmlContent.push(header);
   }
-  if (values[i].type == 'text') {
-    let text = `<p>${values[i].data.text}</p>`;
+  if (values[j].type == 'text') {
+    let text = `<p>${values[j].data.text}</p>`;
     htmlContent.push(text);
   }
 }
-let htmlBlock = htmlContent.join('');
+let htmlBlock = `---
+layout: archive
+title: ${title}
+date: 
+---
+<!--googleon: snippet-->
+<div class="node node-type-news">
+<div class="node-inner">
+<div class="content">
+${htmlContent.join('')}
+</div>
+</div>
+`;
 console.log(htmlBlock);
+
+//create file
+fs.writeFileSync(`${fileName}.html`, htmlBlock, (err) => {
+  console.log(err);
+});
+
+// }
