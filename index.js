d1bc86a789b4f51e4da5df4d2f349c2d85a931a7
codywall <cwall@csumb.edu>
2020-06-29 15:53:14 -0700

    Most of transformation complete

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const walkPath = './pages';
 const cheerio = require('cheerio');
 let pageCounter = 0;
+let modifiedCounter = 0;
 
 const walk = function (dir, done) {
   fs.readdir(dir, function (error, list) {
@@ -23,12 +24,40 @@ const walk = function (dir, done) {
             next();
           });
         } else {
-          // do stuff to file here
+          // modify file here
           let data = fs.readFileSync(file, 'utf-8');
           if (pageCounter === 1) {
-            const $ = cheerio.load(file);
-            console.log(data);
-            console.log('meta div: ' + $('.meta').first().children());
+            const $ = cheerio.load(data, {
+              // stop cheerio from adding head and body tags
+              xmlMode: true,
+            });
+            // remove meta divs
+            let $metaDiv = $('div.meta').first();
+            $metaDiv.find('*').remove();
+            $metaDiv.remove();
+            // remove content divs
+            let contents = $('.node').contents();
+            $('.node').replaceWith(contents);
+            contents = $('.node-inner').contents();
+            $('.node-inner').replaceWith(contents);
+            // turn date into a span
+            $('.news-byline').replaceWith(
+              `<span class="date">${$('.news-byline').html()}</span>`
+            );
+            // strip comments
+            $.root()
+              .contents()
+              .filter(function () {
+                return this.type === 'comment';
+              })
+              .remove();
+            //write updated file
+            fs.writeFileSync(file, $.html(), function (err) {
+              if (err) {
+                throw err;
+              }
+              modifiedCounter++;
+            });
           }
           pageCounter++;
           next();
@@ -49,7 +78,9 @@ walk(walkPath, function (error) {
     console.log(
       '-------------------------------------------------------------'
     );
-    console.log(`finished. ${pageCounter} pages read.`);
+    console.log(
+      `Transformation finished. ${pageCounter} pages read and ${modifiedCounter} pages transformed.`
+    );
     console.log(
       '-------------------------------------------------------------'
     );
