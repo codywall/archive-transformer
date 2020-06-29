0786106d080b49c9c129976168dc8670cb465e66
codywall <cwall@csumb.edu>
2020-06-29 10:39:44 -0700

    Add build message

--- index.js
+++ index.js
@@ -4,29 +4,39 @@ const fs = require('fs');
 let rawData = fs.readFileSync('pages.json');
 let pages = JSON.parse(rawData).pages;
 let htmlContent = [];
-// for (let i = 0; i < pages.length; i++) {
-let content = JSON.parse(pages[0].node.pageContent);
-let values = Object.values(content.blocks);
-let title = pages[0].node.title;
-let fileName = title.replace(/\s+/g, '-').toLowerCase();
+let pageCounter = 0;
+for (let i = 0; i < pages.length; i++) {
+  let content = JSON.parse(pages[i].node.pageContent);
+  let values = Object.values(content.blocks);
+  let title = pages[i].node.title;
+  let fileName = title
+    .toLowerCase()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]+/g, '');
 
-for (let j = 0; j < values.length; j++) {
-  if (values[j].type == 'heading') {
-    let header = values[j].data.url
-      ? `<h${values[j].data.level}><a href='${values[j].data.url}'>${values[j].data.text}</a></h${values[i].data.level}>`
-      : `<h${values[j].data.level}>${values[j].data.text}</h${values[j].data.level}>`;
+  for (let j = 0; j < values.length; j++) {
+    if (values[j].type == 'heading') {
+      let header = values[j].data.url
+        ? `<h${values[j].data.level}><a href='${values[j].data.url}'>${values[j].data.text}</a></h${values[j].data.level}>`
+        : `<h${values[j].data.level}>${values[j].data.text}</h${values[j].data.level}>`;
 
-    htmlContent.push(header);
+      htmlContent.push(header);
+    }
+    if (values[j].type == 'text') {
+      let text = `<p>${values[j].data.text}</p>`;
+      htmlContent.push(text);
+    }
+    // if (values[j].type == 'image') {
+    //   imageCounter++;
+    //   let img = `<img src='${values[j].data.image.url}' alt='${values[j].data.description}'/>`;
+    //   console.log(img);
+    //   htmlContent.push(img);
+    // }
   }
-  if (values[j].type == 'text') {
-    let text = `<p>${values[j].data.text}</p>`;
-    htmlContent.push(text);
-  }
-}
-let htmlBlock = `---
+  let htmlBlock = `---
 layout: archive
 title: ${title}
-date: 
+date: January 01 2020
 ---
 <!--googleon: snippet-->
 <div class="node node-type-news">
@@ -36,11 +46,11 @@ ${htmlContent.join('')}
 </div>
 </div>
 `;
-console.log(htmlBlock);
 
-//create file
-fs.writeFileSync(`pages/${fileName}.html`, htmlBlock, (err) => {
-  console.log(err);
-});
-
-// }
+  //   create files
+  fs.writeFileSync(`pages/${fileName}.html`, htmlBlock, (err) => {
+    console.log(err);
+    pageCounter++;
+  });
+}
+console.log(`Built ${pageCounter} pages.`);
